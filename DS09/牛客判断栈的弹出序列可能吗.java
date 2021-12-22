32b7a0d2bc1eb9d75d7938942697548a344ae399
Telesporter <51327163+Telesporter@users.noreply.github.com>
2021-12-22 22:42:03 +0800

    Add files via upload

---
+++ DS09/牛客判断栈的弹出序列可能吗.java
@@ -0,0 +1,23 @@
+package DataStructure.DS09;
+
+import java.util.Stack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 13498
+ * Date: 2021/12/22
+ * Time: 17:09
+ * Description: https://www.nowcoder.com/practice/d77d11405cc7470d82554cb392585106?tpId=13&tqId=23290&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ */
+public class 牛客判断栈的弹出序列可能吗 {
+
+}
+class Solution {
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        Stack<Integer> stack = new Stack<>();
+        int p1 = 0;
+        int p2 = 0;
+        while (p2 < popA.length)
+
+    }
+}
